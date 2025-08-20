5eb3242cbe0a9868eeda80def5200d2adde9e97f
Adarsha <157779079+adarsha001@users.noreply.github.com>
2025-08-20 09:29:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -312,7 +312,7 @@ export default function App() {
           className="px-4 py-2 bg-green-400/20 text-green-400 rounded hover:bg-green-400/30 transition"
           onClick={() => setCurrentAction("SittingLaugh")}
         >
-          yendi vithi poone
+          laugh
         </button>
         <button
           className="px-4 py-2 bg-green-400/20 text-green-400 rounded hover:bg-green-400/30 transition"
